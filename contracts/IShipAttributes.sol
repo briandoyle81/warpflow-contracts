00f4020beaddede32bad5c7458c9e6919e70cbae
Brian  Doyle <briandoyle81@gmail.com>
2025-10-07 23:19:15 -0400

    Improve Attributes sytem

--- contracts/IShipAttributes.sol
+++ contracts/IShipAttributes.sol
@@ -35,4 +35,49 @@ interface IShipAttributes {
     function getSpecialData(
         Special _special
     ) external view returns (SpecialData memory);
+
+    // Cost calculation functions
+    function calculateShipCost(Ship memory ship) external view returns (uint16);
+
+    function setCosts(Costs memory _costs) external;
+
+    function getCosts() external view returns (uint, Costs memory);
+
+    function getCurrentCostsVersion() external view returns (uint16);
+
+    // Attributes version management functions
+    function setCurrentAttributesVersion(uint16 _version) external;
+
+    function setAttributesVersionBase(
+        uint16 _version,
+        uint8 _baseHull,
+        uint8 _baseSpeed
+    ) external;
+
+    function addGunData(uint16 _version, GunData memory _gunData) external;
+
+    function addArmorData(
+        uint16 _version,
+        ArmorData memory _armorData
+    ) external;
+
+    function addShieldData(
+        uint16 _version,
+        ShieldData memory _shieldData
+    ) external;
+
+    function addSpecialData(
+        uint16 _version,
+        SpecialData memory _specialData
+    ) external;
+
+    function addForeAccuracy(uint16 _version, uint8 _accuracy) external;
+
+    function addEngineSpeed(uint16 _version, uint8 _speed) external;
+
+    function getCurrentAttributesVersion() external view returns (uint16);
+
+    function getAttributesVersionBase(
+        uint16 _version
+    ) external view returns (uint16 version, uint8 baseHull, uint8 baseSpeed);
 }
